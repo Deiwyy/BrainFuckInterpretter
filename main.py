ff85ddb8edde8b5d2aca99b245407660ffb72bed
Dawid Kaprol <69752072+Deiwyy@users.noreply.github.com>
2022-11-23 01:56:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import sys
+
+class Interpretter(object):
+    def __init__(self, code) -> object:
+        self.functions = {
+            '<>': self.move,
+            '+-': self.cellShift,
+            '[]': self.loop,
+            '.,': self.io
+        }
+        self.code = code  # self.stripCode(code)
+        self.loops = []
+        self.cells = [0 for _ in range(30_000)]
+        self.cCell = 0
+        self.currentCodeInd = 0
+    
+    # def stripCode(self, code) -> str:
+    #     strippedCode = ''
+    #     for c in code:
+    #         for fc in self.functions.keys():
+    #             if c in fc: strippedCode += c
+    #     return strippedCode
+
+    def move(self, c):
+        if c == '<':
+            self.cCell -= 1
+            if self.cCell < 0:
+                self.cCell = len(self.cells) - 1
+        if c == '>':
+            self.cCell += 1
+            if self.cCell > len(self.cells) - 1:
+                self.cCell = 0
+
+    def cellShift(self, c):
+        if c == '+':
+            self.cells[self.cCell] += 1
+        if c == '-':
+            self.cells[self.cCell] -= 1
+
+    def io(self, c):
+        if c == ',':
+            self.cells[self.cCell] = ord(input()[0])
+        if c == '.':
+            print(chr(self.cells[self.cCell]), end='')
+
+
+    def loop(self, c):
+        if c == '[':
+            self.loops.append(self.currentCodeInd)
+        if c == ']':
+            if self.cells[self.cCell] != 0:
+                self.currentCodeInd = self.loops[-1]
+            else:
+                self.loops.pop(len(self.loops)-1)
+
+
+    def run(self):
+        while self.currentCodeInd != len(self.code):
+            c = self.code[self.currentCodeInd]
+            for f in self.functions.keys():
+                if c in f:
+                    #print(f'index: {self.currentCodeInd}  |  c: {c}  |  val: {self.cells[self.cCell]}  |  cell: {self.cCell}')
+                    try:
+                        self.functions[f](c)
+                    except Exception as e:
+                        print(f'Python Exception {e}\nAt index: {self.currentCodeInd} \nOP: {c}\ncell: {self.cCell}\n')
+            self.currentCodeInd += 1
+            
+
+def run() -> None:
+    interpretter = None
+    if len(sys.argv) < 2:
+        print('usage: BF.py [path-to-bf-file]')
+        exit(1)
+    with open(sys.argv[1]) as bfFile:
+        bfCode = bfFile.read()
+        interpretter = Interpretter(bfCode)
+    interpretter.run()
+
+
+if __name__ == '__main__':
+    run()
